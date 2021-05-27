3c6436a7b854f51cdb9269956e4c6599db61b4a9
tanvirch0750 <tanvir0750@yahoo.com>
2021-05-27 21:49:49 +0600

    developing a DOM updating algorithm

--- src/js/controller.js
+++ src/js/controller.js
@@ -25,6 +25,9 @@ const controlRecipes = async () => {
 
     recipeView.renderSpinner();
 
+    // 0) Update results view to mark selected search results
+    resultsView.update(model.getSearchResultsPage());
+
     // 1) Loading Recipe
     await model.loadRecipe(id);
 
@@ -75,7 +78,8 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // update the recipe view
-  recipeView.render(model.state.recipes);
+  // recipeView.render(model.state.recipes);
+  recipeView.update(model.state.recipes);
 };
 
 /**==========================================
